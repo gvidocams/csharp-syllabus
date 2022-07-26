761915936f84572ee457eeca01e79836633f4c02
Gvido <gaca6817@gmail.com>
2022-07-26 18:11:25 +0300

    Finishing and tidying up GravityCalculator task

--- csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/Program.cs
@@ -7,20 +7,19 @@ namespace GravityCalculator
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81;  // Earth's gravity in m/s^2
+            double gravity = -9.81;
             double initialVelocity = 0.0;
             double fallingTime = 11.0;
             double initialPosition = 0.0;
             double finalPosition = CalculateFinalPosition(gravity, fallingTime, initialVelocity, initialPosition);
+
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
         }
-        static double CalculateFinalPosition(double accel, double time, double initVelo, double initPos)
+        static double CalculateFinalPosition(double acceleration, double time, double initVelocity, double initPosistion)
         {
-            //Formula for calculating traveled distance is:
-            //0.5 * acceleration * time^2 + inital velocity * time + inital position
-
-            return 0.5 * (accel * Math.Pow(time, 2)) + initVelo * time + initPos;
+            const double VelocityCoefficient = 0.5;
+            return VelocityCoefficient * (acceleration * Math.Pow(time, 2)) + initVelocity * time + initPosistion;
         }
     }
 }
