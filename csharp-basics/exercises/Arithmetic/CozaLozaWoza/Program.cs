cc8e8ac31db79714c5dcebfbe587e6e4d473d86d
Gvido <gaca6817@gmail.com>
2022-07-26 23:37:35 +0300

    Finished CozaLozaWoza task

---
+++ csharp-basics/exercises/Arithmetic/CozaLozaWoza/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CozaLozaWoza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 110;
+
+            int i;
+            for(i = start; i <= end; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("CozaLoza ");
+                }
+                else if(i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.Write("Coza ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }    
+
+                if(i % 11 == 0) 
+                {
+                    Console.WriteLine(""); 
+                }
+            }
+        }
+    }
+}
