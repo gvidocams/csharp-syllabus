9e256edd18958ef58a9b54d5c7521ca55bf1cf84
Gvido <gaca6817@gmail.com>
2022-07-26 19:33:53 +0300

    Finished task Product1ToN

---
+++ csharp-basics/exercises/Arithmetic/Product1ToN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Product1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get factorial of a number\n");
+
+            Console.Write("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= userInput; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
