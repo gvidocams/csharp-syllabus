e5714e2b536e04c4cd27683f67d140c83d9894ae
Gvido <gaca6817@gmail.com>
2022-07-27 23:43:32 +0300

    Made a 2D array for DisplayClassesAndTeachers task

---
+++ csharp-basics/exercises/TypesAndVariables/DisplayClassesAndTeachers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayClassesAndTeachers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string TopAndBottomLine = "+------------------------------------------------------------+";
+
+            string[,] classes = new string[9, 2]
+            {
+                {"English III", "Ms. Lapan"},
+                {"Precalculus", "Mrs. Gideon"},
+                {"Music Theory", "Mr. Davis"},
+                {"Biotechnology", "Ms. Palmer"},
+                {"Principles of Technology I", "Ms. Garcia"},
+                {"Latin II", "Mrs. Barnett"},
+                {"English III", "Ms. Lapan"},
+                {"English III", "Ms. Lapan"},
+                {"English III", "Ms. Lapan"}
+            };
+        }
+    }
+}
