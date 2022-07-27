63d663eadbb798f9346e8b44933e33708bef3fb6
Gvido <gaca6817@gmail.com>
2022-07-27 22:44:10 +0300

    Finished MoreVariablesAndPrinting task

--- csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/VariablesAndNames/Program.cs
@@ -13,14 +13,14 @@ namespace VariablesAndNames
             int carsNotDriven = cars - drivers;
             int seatsInCarWithoutDriver = seatsInACar - 1;
             int carpool_capacity = seatsInCarWithoutDriver * drivers;
-            double averagePassengersPerCar = passengers / (double)drivers;
+            double averagePassengersPerCar = Math.Round((passengers / (double)drivers), 2);
 
             Console.WriteLine("There are " + cars + " cars available.");
             Console.WriteLine("There are only " + drivers + " drivers available.");
             Console.WriteLine("There will be " + carsNotDriven + " empty cars today.");
             Console.WriteLine("We can transport " + carpool_capacity + " people today.");
             Console.WriteLine("We have " + passengers + " people to carpool today.");
-            Console.WriteLine("We need to put about " + String.Format("{0:0.00}", averagePassengersPerCar) + " people in each car.");
+            Console.WriteLine("We need to put about " + averagePassengersPerCar + " people in each car.");
             Console.ReadKey();
         }
     }
