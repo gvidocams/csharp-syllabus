68f5b728fc58f475d70c851b21bd41807f40f37f
Gvido <gaca6817@gmail.com>
2022-07-28 20:43:38 +0300

    Finished PrintDayInWord task

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PrintDayInWord/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintDayInWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an integer of a day: ");
+            var input = Convert.ToInt16(Console.ReadLine());
+
+            switch(input)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid day");
+                    break;
+            }
+        }
+    }
+}
