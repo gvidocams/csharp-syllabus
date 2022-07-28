fb6b815f2371fdcf57e9aa934835dee38413df4f
Gvido <gaca6817@gmail.com>
2022-07-29 00:43:24 +0300

    Tidied Exercise2 task

--- csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise2/Program.cs
@@ -4,8 +4,6 @@ namespace Exercise2
 {
     class Program
     {       
-        // TODO: Write a C# program to sum values of an array.
-        // Array values are starting from min (including) till max (including) number. 
         private static void Main(string[] args)
         {
             var sum = 0;
