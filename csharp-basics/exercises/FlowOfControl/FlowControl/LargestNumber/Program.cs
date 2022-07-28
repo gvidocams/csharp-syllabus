1e378165d957f5d12744ab671dda88b3f61fc390
Gvido <gaca6817@gmail.com>
2022-07-28 20:03:16 +0300

    Finished LargestNumber task

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -4,24 +4,38 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
-            Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            Console.Write("Input the 1st number: ");
+            var input1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            Console.Write("Input the 2nd number: ");
+            var input2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            Console.Write("Input the 3rd number: ");
+            var input3 = Convert.ToInt32(Console.ReadLine());
+
+            if(input1 > input2)
+            {
+                if(input1 > input3)
+                {
+                    Console.WriteLine($"Your first input is the largest: {input1}");
+                }
+                else
+                {
+                    Console.WriteLine($"Your third input is the largest: {input3}");
+                }
+            } 
+            else if(input2 > input3)
+            {
+                Console.WriteLine($"Your second input is the largest: {input2}");
+            }
+            else
+            {
+                Console.WriteLine($"Your thirs input is the largest: {input3}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
