57099d224a109c6dd79c16fec9b4a2098785e936
Gvido <gaca6817@gmail.com>
2022-08-02 22:46:05 +0300

    Finished Exercise 11 task

---
+++ csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a sentence: ");
+            string userSentence = Console.ReadLine();
+
+            Console.WriteLine(ReverseCase(userSentence));
+        }
+
+        static string ReverseCase(string sentence)
+        {
+            char[] letters = sentence.ToCharArray();
+            string result = "";
+            foreach(char letter in letters)
+            {
+                if(letter == char.ToUpper(letter))
+                {
+                    result += char.ToLower(letter);
+                }
+                else
+                {
+                    result += char.ToUpper(letter);
+                }
+            }
+
+            return result;
+        }
+    }
+}
