9801a66f4e0decb4652ad8d97dcb14f8aeb71ab9
Gvido <gaca6817@gmail.com>
2022-08-02 14:39:14 +0300

    Finished Exercise2 task

--- csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -8,15 +8,17 @@ namespace Exercise2
         {
             int i, n;
             
-            Console.WriteLine("Input number of terms : ");
+            Console.Write("Input number of terms: ");
             n = Convert.ToInt32(Console.ReadLine());
             /*
             todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.Pow()
             */
             for (i = 0; i < n; i++)
             {
+                i *= i;
             }
 
+            Console.WriteLine($"1 to the power of {n} is: {i}");
             Console.ReadKey();
         }
     }
