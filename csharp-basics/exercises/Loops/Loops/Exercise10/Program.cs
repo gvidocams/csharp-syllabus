bdec4d324a16f271d030a65388103e92fbce5c74
Gvido <gaca6817@gmail.com>
2022-08-02 22:11:07 +0300

    Finished Exercise10 task

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise10
+{
+    internal class NumberSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Min? ");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Max? ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int index = min;
+
+            for (int i = index; i <= max; i++)
+            {
+                int line = index;
+                for (int j = min; j <= max; j++)
+                {
+                    
+                    if(line > max)
+                    {
+                        line = min;
+                    }
+                    Console.Write(line);
+                    line++;
+                }
+                index++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
