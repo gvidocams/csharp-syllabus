15b3ab3a89d286d2f0b4e794a67ef1b42e815843
Gvido <gaca6817@gmail.com>
2022-08-02 14:48:44 +0300

    Finished Exercise3 task

---
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a position from range (0 - 19): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] randomArray = CreateRandomArray();
+
+            Console.WriteLine($"The value from your input is {randomArray[n]}");
+        }
+
+        static int[] CreateRandomArray()
+        {
+            Random rand = new Random();
+            int arrayLength = 20;
+            int[] array = new int[arrayLength];
+
+            for(int i = 0; i < arrayLength; i++)
+            {
+                array[i] = rand.Next(100);
+            }
+
+            return array;
+        }
+    }
+}
