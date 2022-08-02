dd00efdb84678e9cbeace28d9896fd40c5d7ba39
Gvido <gaca6817@gmail.com>
2022-08-02 15:06:50 +0300

    Finished Exercise5 task

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the chapter: ");
+            string chapter = Console.ReadLine();
+
+            Console.Write("Input the page number: ");
+            string page = Console.ReadLine();
+
+            const int MaxLineLength = 30;
+
+            int usedLength = chapter.Length + page.Length;
+            int dotsLength = MaxLineLength - usedLength;
+
+            Console.Write(chapter);
+
+            for(int i = 0; i < dotsLength; i++)
+            {
+                Console.Write(".");
+            }
+
+            Console.Write($"{page}\n");
+        }
+    }
+}
