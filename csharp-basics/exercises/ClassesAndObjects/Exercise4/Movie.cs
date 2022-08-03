51dfb49c2e25d50278a33f85d2e945da315b4572
Gvido <gaca6817@gmail.com>
2022-08-03 19:37:21 +0300

    Finished Exercise4 task

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Movie
+    {
+        string title;
+        string studio;
+        string rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = "PG";
+        }
+
+        public string GetPG()
+        {
+            return this.rating;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie CasinoRoyale = new Movie("Casino Royale", "Eon Peoductions", "PG13");
+            Movie Glass = new Movie("Glass", "Buena Vista International", "PG13");
+            Movie SpiderMan = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures");
+        }
+    }
+}
