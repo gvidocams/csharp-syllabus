43e6e87aa571be44b5ce7fa195a40c23a1c67c64
Gvido <gaca6817@gmail.com>
2022-08-03 18:55:54 +0300

    Finished Exercise3 task

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Exercise3
+{
+    public class Odometer
+    {
+        int mileage;
+        FuelGauge fuelGauge;
+
+        public Odometer(int mileage, FuelGauge fuelGauge)
+        {
+            this.mileage = mileage;
+            this.fuelGauge = fuelGauge;
+        }
+
+        public void ReportMileage()
+        {
+            Console.WriteLine($"Current mileage {mileage}");
+        }
+
+        public void IncrementMileage()
+        {
+            if (mileage > 999999)
+            {
+                mileage = 0;
+            }
+
+            if (fuelGauge.ReportFuel() > 0)
+            {
+                mileage++;
+                if (mileage % 10 == 0)
+                {
+                    fuelGauge.BurnFuel();
+                }
+            }
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/Exercise3/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Program.cs
@@ -10,6 +10,30 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
+            FuelGauge FuelGauge = new FuelGauge(50);
+            Odometer Odometer = new Odometer(0, FuelGauge);
+
+            Console.WriteLine(FuelGauge.ReportFuel());
+            Odometer.ReportMileage();
+            Console.WriteLine();
+
+            for (int i = 0; i < 100; i++)
+            {
+                FuelGauge.AddFuel();
+            }
+
+            Console.WriteLine(FuelGauge.ReportFuel());
+            Odometer.ReportMileage();
+            Console.WriteLine();
+
+            for (int i = 0; i < 900; i++)
+            {
+                Odometer.IncrementMileage();
+            }
+
+            Console.WriteLine(FuelGauge.ReportFuel());
+            Odometer.ReportMileage();
+            Console.WriteLine();
         }
     }
 }
