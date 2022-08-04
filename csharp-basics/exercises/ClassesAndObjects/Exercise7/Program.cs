3f2a1772b5f33dc4d6749b3a9f0a666c2bb38f39
Gvido <gaca6817@gmail.com>
2022-08-04 19:43:28 +0300

    Finished Exercise7 task

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much money is in the account?: ");
+            SavingsAccount MyAccount = new SavingsAccount(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.Write("Enter the annual interest rate: ");
+            decimal ANNUAL_INTEREST_RATE = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("How long has the account been opened: ");
+            int months = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= months; i++)
+            {
+                Console.Write($"\nEnter amount deposited for month {i}: ");
+                decimal deposit = Convert.ToDecimal(Console.ReadLine());
+                MyAccount.Deposit(deposit);
+
+                Console.Write($"Enter amount withdrawn for month {i}: ");
+                decimal withdraw = Convert.ToDecimal(Console.ReadLine());
+                MyAccount.Withdraw(withdraw);
+
+                MyAccount.AddMonthlyInterest(ANNUAL_INTEREST_RATE);
+            }
+
+            Console.WriteLine($"\nTotal deposited: ${MyAccount.ReportDeposited()}");
+            Console.WriteLine($"Total withdrawn: ${MyAccount.ReportWithdrawn()}");
+            Console.WriteLine($"Interest earned: ${MyAccount.ReportInterestSum()}");
+            Console.WriteLine($"Ending balance: ${MyAccount.ReportBalance()}");
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/README.md
+++ csharp-basics/exercises/ClassesAndObjects/README.md
@@ -226,14 +226,19 @@ Output should be similar to this:
 How much money is in the account?: 10000
 Enter the annual interest rate:5
 How long has the account been opened? 4
-Enter amount deposited for month: 1: 100
-Enter amount withdrawn for 1: 1000
-Enter amount deposited for month: 2: 230
-Enter amount withdrawn for 2: 103
-Enter amount deposited for month: 3: 4050
-Enter amount withdrawn for 3: 2334
-Enter amount deposited for month: 4: 3450
-Enter amount withdrawn for 4: 2340
+
+Enter amount deposited for month 1: 100
+Enter amount withdrawn for month 1: 1000
+
+Enter amount deposited for month 2: 230
+Enter amount withdrawn for month 2: 103
+
+Enter amount deposited for month 3: 4050
+Enter amount withdrawn for month 3: 2334
+
+Enter amount deposited for month 4: 3450
+Enter amount withdrawn for month 4: 2340
+
 Total deposited: $7,830.00
 Total withdrawn: $5,777.00
 Interest earned: $29,977.72
