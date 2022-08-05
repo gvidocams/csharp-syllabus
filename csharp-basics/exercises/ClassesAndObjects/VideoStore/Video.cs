b3a43a8dfd3578664ef793874aad5d73e95f3e7e
Gvido <gaca6817@gmail.com>
2022-08-05 12:12:04 +0300

    Finished VideoStore task

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,44 +1,52 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VideoStore
 {
     class Video
     {
+        public string title;
+        public bool flag;
+        public List<double> ratings;
+
         public Video(string title)
         {
-            
+            this.title = title;
+            flag = true;
+            ratings = new List<double>();
         }
 
         public void BeingCheckedOut()
         {
-            
+            flag = false;
         }
 
         public void BeingReturned()
         {
-            
+            flag = true;
         }
 
         public void ReceivingRating(double rating)
         {
-            
+            ratings.Add(rating);
         }
 
         public double AverageRating()
         {
-            return 0;
+            return ratings.Sum() / ratings.Count;
         }
 
         public bool Available()
         {
-            return true;
+            return flag;
         }
 
-        public string Title => "";
+        public string Title => this.title;
 
         public override string ToString()
         {
-            return $"{Title} {AverageRating()} {Available()}";
+            return $"Title: \"{Title}\" | Average rating: {AverageRating()} | Is available: {Available()}";
         }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -5,34 +5,57 @@ namespace VideoStore
 {
     class VideoStore
     {
+        List<Video> inventory;
 
         public VideoStore()
         {
-            
+            inventory = new List<Video>();
         }
 
         public void AddVideo(string title)
         {
-            
+            inventory.Add(new Video(title));
         }
         
         public void Checkout(string title)
         {
-
+            for(int i = 0; i < inventory.Count; i++)
+            {
+                if(inventory[i].title == title)
+                {
+                    inventory[i].BeingCheckedOut();
+                }
+            }
         }
 
         public void ReturnVideo(string title)
         {
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                if (inventory[i].title == title)
+                {
+                    inventory[i].BeingReturned();
+                }
+            }
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-            
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                if (inventory[i].title == title)
+                {
+                    inventory[i].ReceivingRating(rating);
+                }
+            }
         }
 
         public void ListInventory()
         {
-            
+            foreach(Video video in inventory)
+            {
+                Console.WriteLine(video.ToString());
+            }
         }
     }
 }
