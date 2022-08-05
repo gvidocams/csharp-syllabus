6ae834d7bcf763c990a941b691dc5c4dd75c1a2a
Gvido <gaca6817@gmail.com>
2022-08-06 00:08:37 +0300

    Finished Exercise14 task

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WeekdayInDutch(1970, 9, 21));
+            Console.WriteLine(WeekdayInDutch(1945, 9, 2));
+            Console.WriteLine(WeekdayInDutch(2001, 9, 11));
+        }
+
+        static string WeekdayInDutch(int year, int month, int day)
+        {
+            var cultureInfo = new CultureInfo("nl-NL");
+            DateTime date = new DateTime(year, month, day);
+
+            return cultureInfo.DateTimeFormat.GetDayName(date.DayOfWeek);
+        }
+    }
+}
