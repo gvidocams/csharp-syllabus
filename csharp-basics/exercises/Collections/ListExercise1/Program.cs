4f372d59f7a26add6b003dda57d3a8ff4713a0ad
Gvido <gaca6817@gmail.com>
2022-08-06 16:50:41 +0300

    Finished ListExercise1 task

--- csharp-basics/exercises/Collections/ListExercise1/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise1/Program.cs
@@ -12,8 +12,15 @@ namespace ListExercise1
         {
             //TODO: Write a C# program to create a new list, add some elements (string) and print out the collection.
             //TODO: Add 5 colors to list
+            var colors = new List<string> { "Yellow", "Brown" };
 
-            Console.WriteLine();
+            Console.WriteLine(String.Join(" ", colors));
+
+            colors.Add("Red");
+            colors.Add("Green");
+            colors.Add("Blue");
+            colors.Add("Purple");
+            colors.Add("Magenta");
         }
     }
 }
