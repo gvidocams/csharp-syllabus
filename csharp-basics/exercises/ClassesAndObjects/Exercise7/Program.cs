5b0d7731e2c6afa5a2d6e0c923f95b213f7c9103
Gvido <gaca6817@gmail.com>
2022-08-06 14:25:12 +0300

    Added 2 new methods for Exercise7

--- csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
@@ -8,34 +8,46 @@ namespace Exercise7
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Write("How much money is in the account?: ");
             SavingsAccount MyAccount = new SavingsAccount(Convert.ToDecimal(Console.ReadLine()));
 
             Console.Write("Enter the annual interest rate: ");
-            decimal ANNUAL_INTEREST_RATE = Convert.ToDecimal(Console.ReadLine());
+            decimal _annualInterestRate = Convert.ToDecimal(Console.ReadLine());
 
             Console.Write("How long has the account been opened: ");
             int months = Convert.ToInt32(Console.ReadLine());
 
             for(int i = 1; i <= months; i++)
             {
-                Console.Write($"\nEnter amount deposited for month {i}: ");
-                decimal deposit = Convert.ToDecimal(Console.ReadLine());
-                MyAccount.Deposit(deposit);
+                MyAccount.Deposit(HowMuchDeposited(i));
 
-                Console.Write($"Enter amount withdrawn for month {i}: ");
-                decimal withdraw = Convert.ToDecimal(Console.ReadLine());
-                MyAccount.Withdraw(withdraw);
+                MyAccount.Withdraw(HowMuchWithdrawn(i));
 
-                MyAccount.AddMonthlyInterest(ANNUAL_INTEREST_RATE);
+                MyAccount.AddMonthlyInterest(_annualInterestRate);
             }
 
             Console.WriteLine($"\nTotal deposited: ${MyAccount.ReportDeposited()}");
             Console.WriteLine($"Total withdrawn: ${MyAccount.ReportWithdrawn()}");
             Console.WriteLine($"Interest earned: ${MyAccount.ReportInterestSum()}");
             Console.WriteLine($"Ending balance: ${MyAccount.ReportBalance()}");
+        }
+
+        static decimal HowMuchDeposited(int month)
+        {
+            Console.Write($"\nEnter amount deposited for month {month}: ");
+            decimal deposit = Convert.ToDecimal(Console.ReadLine());
+
+            return deposit;
+        }
+
+        static decimal HowMuchWithdrawn(int month)
+        {
+            Console.Write($"Enter amount withdrawn for month {month}: ");
+            decimal withdraw = Convert.ToDecimal(Console.ReadLine());
+
+            return withdraw;
         }
     }
 }
