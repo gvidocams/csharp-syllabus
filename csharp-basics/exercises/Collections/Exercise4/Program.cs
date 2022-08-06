d85dd6d73cbc2f2bfaa0601a87d1af1be1717497
Gvido <gaca6817@gmail.com>
2022-08-06 22:00:31 +0300

    Finish Exercise4 task

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+
+            while(true)
+            {
+                Console.Write("Enter name: ");
+                var userInput = Console.ReadLine();
+
+                if(userInput == "")
+                {
+                    break;
+                }
+
+                names.Add(userInput);
+            }
+
+            Console.WriteLine($"Unique name list contains: {String.Join(" ", names)}");
+        }
+    }
+}
