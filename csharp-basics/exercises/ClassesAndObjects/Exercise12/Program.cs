d38433157b0553f942fb50e4c9995b67292d2dda
Gvido <gaca6817@gmail.com>
2022-08-06 13:53:35 +0300

    Changed from Testpaper to var in Exercise12

--- csharp-basics/exercises/ClassesAndObjects/Exercise12/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/Program.cs
@@ -10,12 +10,12 @@ namespace Exercise12
     {
         static void Main(string[] args)
         {
-            Testpaper paper1 = new Testpaper("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
-            Testpaper paper2 = new Testpaper("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
-            Testpaper paper3 = new Testpaper("Computing", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
+            var paper1 = new Testpaper("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
+            var paper2 = new Testpaper("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
+            var paper3 = new Testpaper("Computing", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
 
-            Student student1 = new Student();
-            Student student2 = new Student();
+            var student1 = new Student();
+            var student2 = new Student();
 
             Console.WriteLine(String.Join(" ", student1.TestsTaken()));
             student1.TakeTest(paper1, new string[] { "1A", "2D", "3D", "4A", "5A" });
