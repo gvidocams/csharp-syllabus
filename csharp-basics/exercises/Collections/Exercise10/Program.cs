1cf27da74b8629b633b889710add98b5a781a3e0
Gvido <gaca6817@gmail.com>
2022-08-07 14:18:17 +0300

    Finish Exercise10 task

---
+++ csharp-basics/exercises/Collections/Exercise10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSet = new HashSet<string>
+            {
+                "Gvido",
+                "Andis",
+                "Cams",
+                "Codelex",
+                "Code"
+            };
+
+            Console.WriteLine(String.Join(" ", hashSet));
+            
+            hashSet.Clear();
+
+            hashSet.Add("Gvido");
+            hashSet.Add("Cams");
+            hashSet.Add("Code");
+            hashSet.Add("Cams");
+            hashSet.Add("Cams");
+
+            Console.WriteLine(String.Join(" ", hashSet));
+        }
+    }
+}
